be5e765568806c37fece572fe20ecf2052eb8fa8
kachers <renarskacerovskis@gmail.com>
2023-12-06 18:45:57 +0200

    Basic layout completed

---
+++ DeviceMonitoring/Controllers/DevicesController.cs
@@ -0,0 +1,34 @@
+﻿using DeviceMonitoring.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeviceMonitoring.Controllers
+{
+    [ApiController]
+    [Route("Devices")]
+    public class DevicesController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public IEnumerable<Device> Get()
+        {
+            return
+        }
+
+        [HttpPost("Add")]
+        public IEnumerable<Device> Add()
+        {
+            return
+        }
+
+        [HttpPut("Update")]
+        public IEnumerable<Device> Update()
+        {
+            return
+        }
+
+        [HttpPut("Remove")]
+        public IEnumerable<Device> Remove()
+        {
+            return
+        }
+    }
+}
