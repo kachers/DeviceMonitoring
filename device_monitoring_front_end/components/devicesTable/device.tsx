d980ee089aa8ce27e06fa93cc2f07b6dcb78cf0a
kachers <renarskacerovskis@gmail.com>
2023-12-09 23:58:02 +0200

    table text style update

--- device_monitoring_front_end/components/devicesTable/device.tsx
+++ device_monitoring_front_end/components/devicesTable/device.tsx
@@ -11,11 +11,11 @@ const Device: React.FC = () => {
               <p className={styles.locationOffline}>Connection: 0%</p>
             </div>
             <div className={styles.secondColmn}>
-              <p>Model</p>
-              <p>2N Verso</p>
+              <p className='text-neutral-700 font-inter text-xs font-normal leading-16 tracking-tight'>Model</p>
+              <p className='text-neutral-800 font-inter text-base font-medium leading-22 tracking-tighter'>2N Verso</p>
             </div>
             <div className={styles.thirdColumn}>
-              <p>Con-stat</p>
+              <p className='text-neutral-700 font-inter text-xs font-normal leading-16 tracking-tight'>Con-stat</p>
               <p>235/235 messages over 28 days</p>
             </div>
          </div>
