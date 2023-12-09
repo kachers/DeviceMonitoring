dff8984fc8efd8e9059002e034941fa583ee2323
kachers <renarskacerovskis@gmail.com>
2023-12-09 23:52:21 +0200

    Table layout completed

--- device_monitoring_front_end/components/devicesTable/device.tsx
+++ device_monitoring_front_end/components/devicesTable/device.tsx
@@ -10,6 +10,14 @@ const Device: React.FC = () => {
             </p>
               <p className={styles.locationOffline}>Connection: 0%</p>
             </div>
+            <div className={styles.secondColmn}>
+              <p>Model</p>
+              <p>2N Verso</p>
+            </div>
+            <div className={styles.thirdColumn}>
+              <p>Con-stat</p>
+              <p>235/235 messages over 28 days</p>
+            </div>
          </div>
          <div className={styles.buttons}></div>     
       </div>
